0d3e23aa0a4a056322503c040141e9996ec67deb
Ambre BOURDARIE <abourdar@e1r12p2.clusters.42paris.fr>
2023-01-05 10:43:57 +0100

    corrections

--- srcs/requirements/nginx/entrypoint-container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Run NGINX
-nginx -g "daemon off;"
-
-
-tail -f /var/log/nginx/access.log /var/log/nginx/error.log
--- srcs/requirements/wordpress/conf/auto_conf.sh
+++ srcs/requirements/wordpress/conf/auto_conf.sh
@@ -3,20 +3,24 @@
 mkdir -p /var/www/wordpress
 sleep 10
 cd /var/www/wordpress
-wp core download --allow-root
-wp config create	--allow-root \
-                    --dbname=$SQL_DATABASE \
-                    --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD \
-                    --dbhost=mariadb:3306
-wp core install --allow-root \
-                --url=$URL \
-                --title=$TITLE \
-                --admin_user=$ADMIN_USER \
-                --admin_password=$ADMIN_PASSWORD \
-                --admin_email=$ADMIN_MAIL
-wp user create --allow-root bob bob@example.com --role=subscriber
 
-chown -R www-data:www-data /var/www/wordpress
+if [[ ! -f "wp-config.php" ]]
+then
+    wp core download --allow-root
+    wp config create	--allow-root \
+                        --dbname=$SQL_DATABASE \
+                        --dbuser=$SQL_USER \
+                        --dbpass=$SQL_PASSWORD \
+                        --dbhost=mariadb:3306
+    wp core install --allow-root \
+                    --url=$URL \
+                    --title=$TITLE \
+                    --admin_user=$ADMIN_USER \
+                    --admin_password=$ADMIN_PASSWORD \
+                    --admin_email=$ADMIN_MAIL
+    wp user create --allow-root bob bob@example.com --role=subscriber
+    chown -R www-data:www-data /var/www/wordpress
+
+fi
 
 exec "$@"
