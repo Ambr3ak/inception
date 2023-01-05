33fb3c81825559d3b9f882291d64309a5c800f3b
Ambre BOURDARIE <abourdar@e1r12p2.clusters.42paris.fr>
2023-01-05 11:16:56 +0100

    corrections

--- srcs/requirements/wordpress/conf/auto_conf.sh
+++ srcs/requirements/wordpress/conf/auto_conf.sh
@@ -19,6 +19,7 @@ then
                     --admin_password=$ADMIN_PASSWORD \
                     --admin_email=$ADMIN_MAIL
     wp user create --allow-root bob bob@example.com --role=subscriber
+    wp theme install twentysixteen --activate
     chown -R www-data:www-data /var/www/wordpress
 
 fi
